b9cbaa3201b55e12470eb7a031df794aea07547f
M.Amin Rayej <m.amin.rayej@gmail.com>
2024-04-10 21:43:37 +0300

    fix handing issue

--- Zend/zend_portability.h
+++ Zend/zend_portability.h
@@ -397,7 +397,7 @@ char *alloca();
 # define XtOffsetOf(s_type, field) offsetof(s_type, field)
 #endif
 
-#ifndef ZEND_WIN32
+#if defined(HAVE_SIGSETJMP) && !defined(__wasm32)
 # define SETJMP(a) sigsetjmp(a, 0)
 # define LONGJMP(a,b) siglongjmp(a, b)
 # define JMP_BUF sigjmp_buf
