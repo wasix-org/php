b7fd8cf7ee5beb665b3895ef628897a513996654
Arshia Ghafoori <arshia001@live.com>
2024-07-08 07:24:50 +0000

    WIP: Add WASIX tests

---
+++ wasix-test-server.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env sh
+
+set -eou
+
+~/repos/wasmer/wasmer/target/release/wasmer run ./sapi/cli/php.wasm \
+  --singlepass \
+  --net \
+  --mapdir /app:./wasix-tests \
+  --mapdir /openssl:../php-wasix-deps/openssl \
+  -- \
+  -S localhost:8080 \
+  -t /app
