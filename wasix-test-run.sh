c547e2b4f22daacbf7eaf3d81ea78ad159525206
Arshia Ghafoori <arshia001@live.com>
2024-07-08 10:20:03 +0000

    Add more tests

---
+++ wasix-test-run.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+set -u
+
+TEST_SERVER=${TEST_SERVER:-localhost:8080}
+
+ALL_TESTS=( \
+  # sqlite tests currently failing due to an issue with the FS
+  # sqlite \
+  # sqlite-pdo \
+  mysql \
+  mysql-pdo \
+  pgsql \
+  pgsql-pdo \
+  ssl-over-fopen \
+  curl \
+  gd \
+)
+
+FAILED_TESTS=()
+
+for TEST in ${ALL_TESTS[@]}; do
+  OUT=$(curl -s $TEST_SERVER?test=$TEST)
+  if [ "$?" != "0" ] || [ "$OUT" != "Success" ]; then
+    FAILED_TESTS+=("Test $TEST failed with output: $OUT")
+  fi
+done
+
+if (( ${#FAILED_TESTS[@]} == 0 )); then
+  echo "All tests passed!"
+else
+  for FAIL in "${FAILED_TESTS[@]}"; do
+    echo $FAIL
+  done
+  exit -1
+fi
