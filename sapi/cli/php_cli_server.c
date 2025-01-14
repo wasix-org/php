7cc9ab42171409d3f118126c39f054615ddd737d
Arshia Ghafoori <arshia001@live.com>
2025-01-14 16:42:52 +0400

    WIP: EH build + less asyncification + explicit proc_snapshot

--- sapi/cli/php_cli_server.c
+++ sapi/cli/php_cli_server.c
@@ -2873,6 +2873,10 @@ int do_cli_server(int argc, char **argv) /* {{{ */
 	}
 	sapi_module.phpinfo_as_text = 0;
 
+#ifdef __wasi__
+	wasix_proc_snapshot();
+#endif
+
 	{
 		bool ipv6 = strchr(server.host, ':');
 		php_cli_server_logf(
