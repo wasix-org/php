b3eaa1c45f21f3499e188108c9b3ab1834498f16
Arshia Ghafoori <arshia001@live.com>
2025-01-17 11:35:31 +0400

    Fix eh build, make it work with asyncify

--- ne
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-
-set -eou
-
-./sapi/cli/php \
-  -t ../wordpress-wasmer-starter/app \
-  -S localhost:8080 \
-  -d opcache.enable_cli=1 \
-  -d opcache.enable=1
--- wasix-build-eh.sh
+++ wasix-build-eh.sh
@@ -4,11 +4,11 @@ set -eou
 
 make -j16
 
-# TODO: not working correctly
 wasm-opt -O3 \
   --asyncify \
   --pass-arg=asyncify-imports@wasix_32v1.proc_snapshot \
   --pass-arg=asyncify-ignore-indirect \
-  -g \
   -pa max-func-params@32 \
   sapi/cli/php -o sapi/cli/php.wasm
+
+wasm-strip sapi/cli/php.wasm
