6fdad11cdcb5b322325dee4f60aa042f7a7664a3
Arshia Ghafoori <arshia001@live.com>
2025-04-01 21:07:32 +0400

    Rework mail-related configuration

--- ext/standard/mail.c
+++ ext/standard/mail.c
@@ -55,10 +55,6 @@
 		continue;																						\
 	}																									\
 
-#ifdef __wasi__
-# define WASMER_EDGE_MAIL_POSTFIX "@wasmeredgemail.com"
-#endif
-
 extern zend_long php_getuid(void);
 
 static bool php_mail_build_headers_check_field_value(zval *val)
@@ -382,10 +378,12 @@ int wasix_sendmail(const char *host, uint16_t port, const char* username, const 
 #endif
 
 #ifdef __wasi__
+#define NO_REPLY "no-reply@"
 #define MAIL_USERNAME "MAIL_USERNAME"
 #define MAIL_PASSWORD "MAIL_PASSWORD"
 #define MAIL_HOST "MAIL_HOST"
 #define MAIL_PORT "MAIL_PORT"
+#define MAIL_FROM_ADDRESS "MAIL_FROM_ADDRESS"
 #endif
 
 /* {{{ php_mail */
@@ -481,27 +479,40 @@ PHPAPI int php_mail(const char *to, const char *subject, const char *message, co
 	}
 
 #ifdef __wasi__
-	sendmail_from = INI_STR("sendmail_from");
+	// Always prioritize the INI settings over the environment variables
 	smtp = INI_STR("SMTP");
-	username = INI_STR("sendmail_username");
-	password = INI_STR("sendmail_password");
-	smtp_port = INI_INT("smtp_port");
-	if (!smtp_port)
-		smtp_port = 587;
-
-	if (!sendmail_from && !username && !password && !strcmp(smtp, "localhost"))
-	{
-		zval *server_array, *host;
-
+	if (!smtp) {
 		smtp = getenv(MAIL_HOST);
+	}
 
+	username = INI_STR("sendmail_username");
+	if (!username) {
+		username = getenv(MAIL_USERNAME);
+	}
+
+	password = INI_STR("sendmail_password");
+	if (!password) {
+		password = getenv(MAIL_PASSWORD);
+	}
+
+	smtp_port = INI_INT("smtp_port");
+	if (!smtp_port) {
 		char* port = getenv(MAIL_PORT);
 		if (port) {
 			smtp_port = atoi(port);
 		}
+	}
+	if (!smtp_port) {
+		smtp_port = 587; // Default SMTP port
+	}
 
-		username = getenv(MAIL_USERNAME);
-		password = getenv(MAIL_PASSWORD);
+	sendmail_from = INI_STR("sendmail_from");
+	if (!sendmail_from) {
+		sendmail_from = getenv(MAIL_FROM_ADDRESS);
+	}
+	if (!sendmail_from) {
+		// Construct a from address from the host name
+		zval *server_array, *host;
 
 		if ((Z_TYPE(PG(http_globals)[TRACK_VARS_SERVER]) == IS_ARRAY || 
 		     zend_is_auto_global(ZSTR_KNOWN(ZEND_STR_AUTOGLOBAL_SERVER))) &&
@@ -518,30 +529,26 @@ PHPAPI int php_mail(const char *to, const char *subject, const char *message, co
 			// only copy the host part. if not, copy the whole string
 			char* colon_ptr = strchr(host_str, ':');
 			size_t host_len = colon_ptr ? colon_ptr - host_str : strlen(host_str);
-			size_t sendmail_from_len = host_len + strlen(WASMER_EDGE_MAIL_POSTFIX) + 1;
+			size_t sendmail_from_len = strlen(NO_REPLY) + host_len + 1;
 
 			sendmail_from = malloc(sendmail_from_len);
 
 			if (sendmail_from == NULL) {
 				fprintf(stderr, "Memory allocation failed\n");
-				MAIL_RET(1);
+				MAIL_RET(0);
 			}
 
 			sendmail_from_alloc = true;
 
-			strncpy(sendmail_from, host_str, host_len);
-			strcpy(sendmail_from + host_len, WASMER_EDGE_MAIL_POSTFIX);
-		}
-		else
-		{
-			// Always overwrite the from address when sending through the default credentials
-			sendmail_from = "unknown-php-domain@wasmeredgemail.com";
+			strcpy(sendmail_from, NO_REPLY);
+			strncpy(sendmail_from + strlen(NO_REPLY), host_str, host_len);
+			sendmail_from[strlen(NO_REPLY) + host_len] = '\0';
 		}
 	}
 
-	if (!username || !password || !smtp) {
-		fprintf(stderr, "Username, password, or smtp server for mail is not provided\n");
-		MAIL_RET(1);
+	if (!username || !password || !smtp || !sendmail_from) {
+		php_error(E_WARNING, "Username, password, or smtp server for mail not provided");
+		MAIL_RET(0);
 	}
 
 	wasix_sendmail_result = wasix_sendmail(
