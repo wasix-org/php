9f579c8b7efa13d163ac79c6d939e27e20804a91
Arshia Ghafoori <arshia001@live.com>
2025-06-11 15:52:07 +0000

    Update CI scripts

--- wasix-build-eh.sh
+++ wasix-build-eh.sh
@@ -5,7 +5,7 @@ set -eou
 make -j16
 
 wasm-opt -O3 \
-  --experimental-new-eh \
+  --emit-exnref \
   --no-validation \
   --all-features \
   --strip-debug \
@@ -13,4 +13,4 @@ wasm-opt -O3 \
   --pass-arg=asyncify-imports@wasix_32v1.proc_snapshot \
   --pass-arg=asyncify-ignore-indirect \
   --pass-arg=max-func-params@32 \
-  sapi/cli/php -o sapi/cli/php-eh.wasm
+  sapi/cli/php -o sapi/cli/php.wasm
