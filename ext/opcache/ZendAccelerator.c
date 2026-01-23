8afc773d17e1212ae4352a9a2ae55145c04bc4c7
Arshia <arshia001@live.com>
2026-01-23 12:13:31 +0400

    Fix our old changes to the OpCache extension w.r.t. ZTS

--- ext/opcache/ZendAccelerator.c
+++ ext/opcache/ZendAccelerator.c
@@ -2846,6 +2846,7 @@ static inline zend_result accel_find_sapi(void)
 		"fuzzer",
 		"frankenphp",
 		"ngx-php",
+		"phpix",
 		NULL
 	};
 	const char **sapi_name;
@@ -2968,7 +2969,7 @@ void accel_globals_ctor(zend_accel_globals *accel_globals)
 	GC_MAKE_PERSISTENT_LOCAL(accel_globals->key);
 }
 
-static void accel_globals_dtor(zend_accel_globals *accel_globals)
+void accel_globals_dtor(zend_accel_globals *accel_globals)
 {
 	zend_string_free(accel_globals->key);
 	if (accel_globals->preloaded_internal_run_time_cache) {
@@ -3149,11 +3150,6 @@ static void accel_move_code_to_huge_pages(void)
 
 int accel_startup(zend_extension *extension)
 {
-#ifdef ZTS
-	accel_globals_id = ts_allocate_id(&accel_globals_id, sizeof(zend_accel_globals), (ts_allocate_ctor) accel_globals_ctor, (ts_allocate_dtor) accel_globals_dtor);
-#else
-#endif
-
 #ifdef HAVE_JIT
 	zend_jit_init();
 #endif
--- ext/opcache/ZendAccelerator.h
+++ ext/opcache/ZendAccelerator.h
@@ -338,6 +338,9 @@ zend_string* ZEND_FASTCALL accel_new_interned_string(zend_string *str);
 uint32_t zend_accel_get_class_name_map_ptr(zend_string *type_name);
 
 void accel_globals_ctor(zend_accel_globals *accel_globals);
+#ifdef ZTS
+void accel_globals_dtor(zend_accel_globals *accel_globals);
+#endif
 
 END_EXTERN_C()
 
