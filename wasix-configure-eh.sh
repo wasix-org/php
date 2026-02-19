422f5cc223403b292ce508b025231ef1277291b9
Zebreus <zebreus@zebre.us>
2026-02-19 14:58:39 +0100

    Fix exnref build and increase optimization levels

--- wasix-configure-eh.sh
+++ wasix-configure-eh.sh
@@ -48,9 +48,9 @@ export \
   NM="wasixnm" \
   CC="wasixcc" \
   CXX="wasixcc++" \
-  CFLAGS="-g -flto -O2" \
-  CXXFLAGS="-g -flto -O2" \
-  LIBS="-L$PHP_WASIX_DEPS/lib-eh --no-wasm-opt" \
+  CFLAGS="-g -flto -O4" \
+  CXXFLAGS="-g -flto -O4" \
+  LIBS="-L$PHP_WASIX_DEPS/lib-eh --no-wasm-opt -g -flto -O4" \
   WASIXCC_INCLUDE_CPP_SYMBOLS="yes" \
   WASIXCC_WASM_EXCEPTIONS="yes" \
   PROG_SENDMAIL="/usr/bin/sendmail"
@@ -64,7 +64,7 @@ export \
   --enable-bcmath --enable-tidy --enable-gd --enable-exif --with-jpeg --with-freetype --with-webp \
   --enable-fiber-asm --with-curl --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-zip --with-sodium \
   --with-pgsql="$PHP_WASIX_DEPS"/pgsql-eh --with-pdo-pgsql="$PHP_WASIX_DEPS"/pgsql-eh --enable-intl \
-  --with-pdo-sqlite --enable-ftp --enable-igbinary --with-imagick \
+  --with-pdo-sqlite --enable-ftp --enable-igbinary --with-imagick --enable-debug \
   --program-suffix=".wasm"
 
 ./wasix-build-eh.sh
